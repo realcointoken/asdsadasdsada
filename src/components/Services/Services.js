960a1763b2f7c8c1422baca544fc2d67b338a178
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-27 03:23:38 -0300

    Update Services.js

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -6,7 +6,7 @@ function Services(props) {
       id: 1,
       title: "No Fees",
       description:
-        "In-Dex Swap is a multi-chain compatible decentralised exchange aggregator protocol focused on making zero-fee transactions possible. We are aiming to make on-chain trading simple and easy, by providing access to multi-chains like ethereum, BSC under one roof. The core product is being designed to function gasless and focused on aggregating liquidity from multiple protocols.",
+        "In-Dex Sale is a multi-chain compatible decentralised exchange aggregator protocol focused on making zero-fee transactions possible. We are aiming to make on-chain trading simple and easy, by providing access to multi-chains like ethereum, BSC under one roof. The core product is being designed to function gasless and focused on aggregating liquidity from multiple protocols.",
       link: "#",
       img: require("../../assets/images/setting.svg").default,
     },
