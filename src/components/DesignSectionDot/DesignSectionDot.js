f4c8d1c4bd4faf4517343dda1fd2e3f68d39c9bb
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-31 15:06:19 -0300

    Update DesignSectionDot.js

--- src/components/DesignSectionDot/DesignSectionDot.js
+++ src/components/DesignSectionDot/DesignSectionDot.js
@@ -52,6 +52,55 @@ function DesignSectionDot(props) {
         style={{ "--design-bg": "#8C7AE6" }}
         className="design_item"
       ></span>
+<span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="50"
+        style={{ "--design-bg": "#00A8FF" }}
+        className="design_item me-2"
+      ></span>
+      <span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="60"
+        style={{ "--design-bg": "#9C88FF" }}
+        className="design_item me-2"
+      ></span>
+      <span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="70"
+        style={{ "--design-bg": "#FBC531" }}
+        className="design_item  me-2"
+      ></span>
+      <span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="80"
+        style={{ "--design-bg": "#4CD137" }}
+        className="design_item  me-2"
+      ></span>
+      <span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="90"
+        style={{ "--design-bg": "#487EB0" }}
+        className="design_item  me-2"
+      ></span>
+      <span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="100"
+        style={{ "--design-bg": "#E84118" }}
+        className="design_item  me-2"
+      ></span>
+      <span
+        data-aos="fade-right" data-aos-once="true"
+        data-aos-anchor-placement="bottom-bottom"
+        data-aos-delay="110"
+        style={{ "--design-bg": "#8C7AE6" }}
+        className="design_item"
+      ></span>
     </div>
   );
 }
